dc9cb2f575cf827177f627d435bb8513257da0d9
Stefan Ionescu <stefan_alter_ego@protonmail.com>
2021-05-25 13:57:02 +0100

    Fix comment and add licenses

--- src/GebUniswapV3ManagerBase.sol
+++ src/GebUniswapV3ManagerBase.sol
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2021 Reflexer Labs
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 pragma solidity 0.6.7;
 
 import { ERC20 } from "./erc20/ERC20.sol";
@@ -264,7 +288,7 @@ abstract contract GebUniswapV3ManagerBase is ERC20 {
     }
 
     /**
-     * @notice Remove liquidity and withdraw the underlying assets
+     * @notice Add liquidity to this pool manager
      * @param _position The position to perform the operation
      * @param _newLiquidity The amount of liquidity to add
      * @param _targetTick The price to center the position around
